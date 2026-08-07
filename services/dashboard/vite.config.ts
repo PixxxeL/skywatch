0909be64f2601875cd481b4fd683fa7af71077dc
Ivan Sergeev <ivan.n.sergeev@gmail.com>
2026-08-07 08:09:51 +0500

    Add Yandex metrika

--- services/dashboard/vite.config.ts
+++ services/dashboard/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { yandexMetrika } from './metrika'
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), yandexMetrika()],
     server: {
         proxy: {
             '/api': 'http://127.0.0.1:8080',
